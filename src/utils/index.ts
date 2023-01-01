78a971b52aebd88d73541f1cd143f1ae4d17f703
raedatoui <raed.atoui@gmail.com>
2023-01-01 11:09:22 -0500

    shuffle and filter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { useInterval } from './interval';
 export { useWindowSize } from './resize';
 export { useTimeout } from './timeout';
 export { useFetchData } from './fetch';
+export { shuffleList } from './shuffle-list';
